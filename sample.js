f2fb999892a05022238fccdb1f8d29ce1b0e073e
tfuabio <114894082+tfuabio@users.noreply.github.com>
2022-12-03 07:55:56 +0000

    [add] 相手のじゃんけんの手を取得する関数

--- sample.js
+++ sample.js
@@ -1,1 +1,16 @@
-let 
+const user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+alert('あなたの選んだ手は' + user_hand + 'です。');
+
+function getJShand() {
+  let js_hand_num = Math.floor( Math.random() * 3);
+
+  if (js_hand_num == 0) {
+    js_hand = "グー";
+  } else if (js_hand_num == 1) {
+    js_hand = "チョキ";
+  } else if (js_hand_num == 2) {
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
